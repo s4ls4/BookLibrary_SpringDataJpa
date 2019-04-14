63f015235ec094d92fb328640dab162984e27055
Muradasil Birhan <b98lion@gmail.com>
2019-04-14 20:29:55 +0300

    safdg

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -5,7 +5,7 @@ public class main {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context=
                 new AnnotationConfigApplicationContext(
-                        "ro.ubb.springjpa.config"
+                        "config"
                 );
 
         Console console=context.getBean(Console.class);
