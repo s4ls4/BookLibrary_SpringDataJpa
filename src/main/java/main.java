19c63807741fb398a44f0c2fefd451a1147b5b6b
Muradasil Birhan <b98lion@gmail.com>
2019-04-14 17:35:57 +0300

    main

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ui.Console;
 
 public class main {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context=
+                new AnnotationConfigApplicationContext(
+                        "ro.ubb.springjpa.config"
+                );
+
+        Console console=context.getBean(Console.class);
+        console.runConsole();
+
+        System.out.println("hello");
+    }
 }
